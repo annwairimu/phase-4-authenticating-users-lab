3c4dbe8358b0c5aed1615bb31795e46c33b6a7d2
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-07-01 14:49:30 +0300

    done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+# app/controllers/users_controller.rb
+
+class UsersController < ApplicationController
+    def show
+      if session[:user_id]
+        user = User.find(session[:user_id])
+        render json: user
+      else
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
+    end
+  end
+  
